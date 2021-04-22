beead9fd6dc1832e99fb1bc0a6d186657663c92f
huntj88 <huntj88@gmail.com>
2021-04-22 00:55:14 -0500

    cleanup

--- Android/app/src/main/java/me/jameshunt/privatechat/QRScanner.kt
+++ Android/app/src/main/java/me/jameshunt/privatechat/QRScanner.kt
@@ -2,12 +2,10 @@ package me.jameshunt.privatechat
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
@@ -23,8 +21,6 @@ import kotlin.coroutines.suspendCoroutine
 class QRScanner {
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var lastCheck: Instant
-
-    private val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     suspend fun getHashedIdentity(mainActivity: MainActivity): String {
         cameraExecutor = Executors.newSingleThreadExecutor()
@@ -43,57 +39,62 @@ class QRScanner {
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder().build()
-                .apply { setSurfaceProvider(mainActivity.viewFinder.surfaceProvider) }
+            val preview = Preview.Builder().build().apply {
+                setSurfaceProvider(mainActivity.viewFinder.surfaceProvider)
+            }
 
-            val options = BarcodeScannerOptions.Builder()
-                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-                .build()
-
-            val client = BarcodeScanning.getClient(options)
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .apply {
-                    this.setAnalyzer(cameraExecutor, object : ImageAnalysis.Analyzer {
-                        @SuppressLint("UnsafeExperimentalUsageError")
-                        override fun analyze(image: ImageProxy) {
-                            val mediaImage = image.image
-                            val canCheckAgain = lastCheck > Instant.now().minus(2, ChronoUnit.SECONDS)
-                            if (!canCheckAgain || mediaImage == null) {
-                                image.close()
-                                return
-                            }
-
-                            Log.d("scanned", DateTimeFormatter.ISO_INSTANT.format(lastCheck))
-                            lastCheck = Instant.now()
-
-                            client
-                                .process(InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees))
-                                .addOnSuccessListener { barcodes ->
-                                    barcodes.firstOrNull()?.rawValue?.let { hashedIdentity ->
-                                        Log.d("scanned", hashedIdentity)
-                                        onResult(hashedIdentity)
-                                        cameraExecutor.shutdown()
-                                        cameraProviderFuture.cancel(true)
-                                    }
-                                }
-                                .addOnFailureListener { it.printStackTrace() }
-                                .addOnCompleteListener { image.close() }
-                        }
-                    })
-                }
+            val qrAnalyzer = qrAnalyzer(cameraProviderFuture, onResult)
 
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(mainActivity, cameraSelector, preview, imageAnalyzer)
+                cameraProvider.bindToLifecycle(mainActivity, CameraSelector.DEFAULT_BACK_CAMERA, preview, qrAnalyzer)
             } catch (exc: Exception) {
                 Log.e("Blah", "Use case binding failed", exc)
             }
 
         }, ContextCompat.getMainExecutor(mainActivity))
+    }
+
+    private fun qrAnalyzer(
+        cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
+        onResult: (String) -> Unit
+    ): ImageAnalysis {
+        val options = BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
+        val client = BarcodeScanning.getClient(options)
+
+        val analyzer = object : ImageAnalysis.Analyzer {
+            @SuppressLint("UnsafeExperimentalUsageError")
+            override fun analyze(image: ImageProxy) {
+                val mediaImage = image.image
+                val canCheckAgain = lastCheck > Instant.now().minus(2, ChronoUnit.SECONDS)
+                if (!canCheckAgain || mediaImage == null) {
+                    image.close()
+                    return
+                }
+
+                Log.d("scanned", DateTimeFormatter.ISO_INSTANT.format(lastCheck))
+                lastCheck = Instant.now()
+
+                client
+                    .process(InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees))
+                    .addOnSuccessListener { barcodes ->
+                        barcodes.firstOrNull()?.rawValue?.let { hashedIdentity ->
+                            Log.d("scanned", hashedIdentity)
+                            onResult(hashedIdentity)
+                            cameraExecutor.shutdown()
+                            cameraProviderFuture.cancel(true)
+                        }
+                    }
+                    .addOnFailureListener { it.printStackTrace() }
+                    .addOnCompleteListener { image.close() }
+            }
+        }
+
+        return ImageAnalysis.Builder()
+            .build()
+            .apply { this.setAnalyzer(cameraExecutor, analyzer) }
     }
 }
