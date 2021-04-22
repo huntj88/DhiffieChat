0def43d644aa1eb272a8a22dbc9d0ade653dda72
huntj88 <huntj88@gmail.com>
2021-04-21 22:10:33 -0500

    upload files to s3

--- Functions/src/main/kotlin/me/jameshunt/privatechat/SendFile.kt
+++ Functions/src/main/kotlin/me/jameshunt/privatechat/SendFile.kt
@@ -2,13 +2,20 @@ package me.jameshunt.privatechat
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
 import com.fasterxml.jackson.annotation.JsonAlias
+import java.io.ByteArrayInputStream
+import java.security.MessageDigest
+import java.util.*
 
 class SendFile : RequestHandler<Map<String, Any?>, GatewayResponse> {
     override fun handleRequest(request: Map<String, Any?>, context: Context): GatewayResponse {
         return awsTransformAuthed<ByteArray, UserUserQueryParams, Unit>(request, context) { body, params, _ ->
             context.logger.log("file length in bytes: ${body.size}")
             context.logger.log("user user iv: ${params.userUserIv}")
+            val s3 = AmazonS3ClientBuilder.standard().build()
+            s3.putObject("encrypted-file-bucket-z00001", body.toS3Key(), ByteArrayInputStream(body), ObjectMetadata())
         }
     }
 }
@@ -17,3 +24,9 @@ data class UserUserQueryParams(
     @JsonAlias("UserUserIv", "userUserIv")
     val userUserIv: String
 )
+
+fun ByteArray.toS3Key(): String = MessageDigest
+    .getInstance("SHA-256")
+    .digest(this)
+    .let { Base64.getEncoder().encodeToString(it) }
+    .replace("/", "_") // don't make folders
