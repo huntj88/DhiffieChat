15703e87439e39c202773e8bdde31c6b3ac8c4e9
huntj88 <huntj88@gmail.com>
2021-05-03 22:11:31 -0500

    show friends on home screen

--- Android/app/src/main/java/me/jameshunt/privatechat/compose/HomeScreen.kt
+++ Android/app/src/main/java/me/jameshunt/privatechat/compose/HomeScreen.kt
@@ -3,7 +3,6 @@ package me.jameshunt.privatechat.compose
 import LoadingIndicator
 import android.util.Log
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -15,7 +14,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import kotlinx.coroutines.launch
 import me.jameshunt.privatechat.DI
-import me.jameshunt.privatechat.PrivateChatApi.MessageFromUserSummary
+import me.jameshunt.privatechat.PrivateChatApi.*
 import me.jameshunt.privatechat.PrivateChatService
 import me.jameshunt.privatechat.R
 
@@ -27,8 +26,28 @@ class HomeViewModelFactory : ViewModelProvider.Factory {
 
 class HomeViewModel(private val apiService: PrivateChatService) : ViewModel() {
 
-    private val _messageSummaries: MutableLiveData<List<MessageFromUserSummary>?> = MutableLiveData(null)
-    val messageSummaries: LiveData<List<MessageFromUserSummary>?> = _messageSummaries
+    private val _relationships: MutableLiveData<Relationships?> = MutableLiveData(null)
+    private val _messageSummaries: MutableLiveData<List<MessageFromUserSummary>?> =
+        MutableLiveData(null)
+
+    data class FriendMessageData(
+        val from: String,
+        val count: Int
+    )
+
+    val friendMessageData: LiveData<List<FriendMessageData>?> = _relationships.combineWith(
+        _messageSummaries
+    ) { relationships: Relationships?, messageSummaries: List<MessageFromUserSummary>? ->
+        relationships ?: return@combineWith null
+        messageSummaries ?: return@combineWith null
+
+        relationships.friends.map { friendUserId ->
+            messageSummaries
+                .firstOrNull { friendUserId == it.from }
+                ?.let { FriendMessageData(it.from, it.count) }
+                ?: FriendMessageData(from = friendUserId, count = 0)
+        }
+    }
 
     init {
         viewModelScope.launch {
@@ -37,6 +56,7 @@ class HomeViewModel(private val apiService: PrivateChatService) : ViewModel() {
     }
 
     private suspend fun refresh() {
+        _relationships.value = apiService.getUserRelationships()
         _messageSummaries.value = apiService.getMessageSummaries()
     }
 
@@ -59,11 +79,14 @@ fun HomeScreen(navController: NavController) {
         }
 
 
-        val messageSummaries = viewModel.messageSummaries.observeAsState().value
+        val messageSummaries = viewModel.friendMessageData.observeAsState().value
 
         messageSummaries?.let { summaries ->
+            Spacer(modifier = Modifier.height(8.dp))
             summaries.forEach {
-                Text(text = it.from)
+                CallToAction(it.from, R.drawable.ic_baseline_qr_code_scanner_24) {
+                    
+                }
             }
 
         } ?: run {
@@ -73,5 +96,20 @@ fun HomeScreen(navController: NavController) {
             }
         }
     }
+}
+
+
+fun <T, K, R> LiveData<T>.combineWith(
+    liveData: LiveData<K>,
+    block: (T?, K?) -> R
+): LiveData<R> {
+    val result = MediatorLiveData<R>()
+    result.addSource(this) {
+        result.value = block(this.value, liveData.value)
+    }
+    result.addSource(liveData) {
+        result.value = block(this.value, liveData.value)
+    }
+    return result
 }
 
