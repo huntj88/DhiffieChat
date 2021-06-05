7faaf0526fcae580888fa71daa25b184c71f46fd
huntj88 <huntj88@gmail.com>
2021-06-04 22:59:10 -0500

    Cleanup Launcher screen

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/profile/ProfileFragment.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/profile/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package me.jameshunt.dhiffiechat.profile
 
 import androidx.compose.runtime.Composable
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import me.jameshunt.dhiffiechat.compose.ComposeFragment
 import me.jameshunt.dhiffiechat.compose.injectedViewModel
@@ -12,7 +14,11 @@ class ProfileFragment : ComposeFragment() {
     override fun ScreenComposable() {
         UserProfile(
             viewModel = viewModel,
-            onAliasSet = { findNavController(this).popBackStack() }
+            onAliasSet = {
+                findNavController(this).popBackStack()
+                val window = requireActivity().window
+                WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.ime())
+            }
         )
     }
 }
