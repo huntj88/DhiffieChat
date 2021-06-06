2fa2b1da9fa1b4ddec1bd36d76bfc545dc0fab9b
huntj88 <huntj88@gmail.com>
2021-06-05 23:15:57 -0500

    Bare bones minimum nav drawer able to open and close

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/ui/home/HomeFragment.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/ui/home/HomeFragment.kt
@@ -1,9 +1,19 @@
 package me.jameshunt.dhiffiechat.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import me.jameshunt.dhiffiechat.R
+import me.jameshunt.dhiffiechat.openNavDrawer
 import me.jameshunt.dhiffiechat.ui.compose.ComposeFragment
 import me.jameshunt.dhiffiechat.ui.compose.injectedViewModel
 
@@ -12,18 +22,32 @@ class HomeFragment: ComposeFragment() {
 
     @Composable
     override fun ScreenComposable() {
-        HomeScreen(
-            viewModel = viewModel,
-            toUserProfile = { findNavController().navigate(R.id.profileFragment) },
-            toManageFriends = { findNavController().navigate(R.id.manageFriendsFragment) },
-            toShowNextMessage = { fromUserId ->
-                val args = bundleOf("fromUserId" to fromUserId)
-                findNavController().navigate(R.id.showNextMessageFragment, args)
-            },
-            toSendMessage = { toUserId ->
-                val args = bundleOf("toUserId" to toUserId)
-                findNavController().navigate(R.id.send_message, args)
-            },
-        )
+        Column {
+            TopAppBar(
+                title = { Text(text = "wow") },
+                navigationIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_menu_24),
+                        contentDescription = "Navigation",
+                        modifier = Modifier
+                            .padding(all = 8.dp)
+                            .clickable { openNavDrawer() }
+                    )
+                }
+            )
+            HomeScreen(
+                viewModel = viewModel,
+                toUserProfile = { findNavController().navigate(R.id.profileFragment) },
+                toManageFriends = { findNavController().navigate(R.id.manageFriendsFragment) },
+                toShowNextMessage = { fromUserId ->
+                    val args = bundleOf("fromUserId" to fromUserId)
+                    findNavController().navigate(R.id.showNextMessageFragment, args)
+                },
+                toSendMessage = { toUserId ->
+                    val args = bundleOf("toUserId" to toUserId)
+                    findNavController().navigate(R.id.send_message, args)
+                },
+            )
+        }
     }
 }
