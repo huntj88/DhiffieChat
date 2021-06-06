4d28b5ba00d3000007cb88faa32fd8c9105a8ccf
huntj88 <huntj88@gmail.com>
2021-06-05 23:15:57 -0500

    Bare bones minimum nav drawer able to open and close

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/MainActivity.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/MainActivity.kt
@@ -1,11 +1,27 @@
 package me.jameshunt.dhiffiechat
 
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
+}
+
+fun Fragment.openNavDrawer() {
+    activity
+        ?.findViewById<DrawerLayout>(R.id.drawer_layout)
+        ?.openDrawer(GravityCompat.START);
 }
