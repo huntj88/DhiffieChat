412bdeaf5b39c80bfa86c96346582290d18ca523
huntj88 <huntj88@gmail.com>
2021-06-19 14:29:19 -0500

    cleanup navigation menu code

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/NavDrawerHandler.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/NavDrawerHandler.kt
@@ -2,23 +2,67 @@ package me.jameshunt.dhiffiechat
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.annotation.IdRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
 import androidx.customview.widget.Openable
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.navigation.NavigationView
+import me.jameshunt.dhiffiechat.ui.compose.injectedViewModel
+import me.jameshunt.dhiffiechat.ui.home.HomeViewModel
 import java.lang.ref.WeakReference
 
+fun Fragment.openNavDrawer() {
+    (activity as? MainActivity)?.let {
+        val viewModel: HomeViewModel by it.injectedViewModel()
+        it.findViewById<DrawerLayout>(R.id.drawer_layout)
+            ?.openDrawer(GravityCompat.START)
+            .also {
+                activity?.findViewById<TextView>(R.id.navigationAliasText)?.text = viewModel.alias.value?.alias
+            }
+    }
+}
+
 /**
- * Taken from the compose lib, but modified so that some buttons in the navigation UI start a custom task.
+ * Most of this file is Taken from the compose lib, but modified to fit my use cases
  * Duplicated because you can only have one NavigationItemSelectedListener, and existing one isn't exposed in any way
  */
 
-class NavDrawerHandler {
-    fun setupWithNavController(
+class NavDrawerHandler(private val activity: MainActivity) {
+    private val viewModel: HomeViewModel by activity.injectedViewModel()
+
+    fun init() {
+        val navHostFragment = activity.supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        setupWithNavController(
+            navigationView = activity.findViewById<NavigationView>(R.id.nav_view),
+            navController = navHostFragment.navController,
+            onScanSelected = { viewModel.scanSelected() },
+            onShareSelected = { viewModel.shareSelected() }
+        )
+
+        viewModel.alias.observe(activity) {
+            activity.findViewById<TextView>(R.id.navigationAliasText)?.text = it?.alias
+        }
+    }
+
+    fun handleBack(onSuperBackPressed: () -> Unit) {
+        val drawerLayout = activity.findViewById<DrawerLayout>(R.id.drawer_layout)
+        when (drawerLayout.isOpen) {
+            true -> drawerLayout.close()
+            false -> onSuperBackPressed()
+        }
+    }
+
+    private fun setupWithNavController(
         navigationView: NavigationView,
         navController: NavController,
         onScanSelected: () -> Unit,
