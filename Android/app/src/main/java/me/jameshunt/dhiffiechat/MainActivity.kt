2a447d38961330c00ff46b6b3f2ee31b23898409
huntj88 <huntj88@gmail.com>
2021-06-19 14:29:19 -0500

    cleanup navigation menu code

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/MainActivity.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/MainActivity.kt
@@ -1,61 +1,18 @@
 package me.jameshunt.dhiffiechat
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import com.google.android.material.navigation.NavigationView
-import me.jameshunt.dhiffiechat.ui.compose.injectedViewModel
-import me.jameshunt.dhiffiechat.ui.home.HomeViewModel
 
 class MainActivity : FragmentActivity() {
-    private val navDrawerHandler = NavDrawerHandler()
-    private val viewModel: HomeViewModel by injectedViewModel()
+    private val navDrawerHandler = NavDrawerHandler(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        val navController = navHostFragment.navController
-        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
-
-        viewModel.alias.observe(this) {
-            findViewById<TextView>(R.id.navigationAliasText)?.text = it?.alias
-        }
+        navDrawerHandler.init()
     }
 
     override fun onBackPressed() {
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        when (drawerLayout.isOpen) {
-            true -> drawerLayout.close()
-            false -> super.onBackPressed()
-        }
-    }
-
-    private fun NavigationView.setupWithNavController(navController: NavController) {
-        navDrawerHandler.setupWithNavController(
-            navigationView = this,
-            navController = navController,
-            onScanSelected = { viewModel.scanSelected() },
-            onShareSelected = { viewModel.shareSelected() }
-        )
-    }
-}
-
-fun Fragment.openNavDrawer() {
-    (activity as? MainActivity)?.let {
-        val viewModel: HomeViewModel by it.injectedViewModel()
-        it.findViewById<DrawerLayout>(R.id.drawer_layout)
-        ?.openDrawer(GravityCompat.START)
-        .also {
-            activity?.findViewById<TextView>(R.id.navigationAliasText)?.text = viewModel.alias.value?.alias
-        }
+        navDrawerHandler.handleBack { super.onBackPressed() }
     }
 }
