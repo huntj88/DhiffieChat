fef24cc00f0c0ca4f8122517493019bfe15240a3
huntj88 <huntj88@gmail.com>
2021-06-20 17:49:26 -0500

    use commit hash if not on branch for version script

--- Android/appVersion.sh
+++ Android/appVersion.sh
@@ -4,4 +4,9 @@ HEAD_COUNT=$(git rev-list --first-parent --count HEAD)
 COUNT_DIFF=$((HEAD_COUNT-MASTER_COUNT))
 BRANCH_NAME=$(git branch --show-current)
 
+# If not on a branch, use short commit hash
+if [[ "$BRANCH_NAME" == "" ]]; then
+  BRANCH_NAME=$(git rev-parse --short HEAD)
+fi
+
 echo "${MASTER_COUNT}.${COUNT_DIFF}.${BRANCH_NAME}.${DHIFFIE_ENV}"
