6d64672cf9167dd4bebcfa44cfa06a084890e976
huntj88 <huntj88@gmail.com>
2021-06-21 17:08:02 -0500

    simplify appVersion.sh

--- Android/appVersion.sh
+++ Android/appVersion.sh
@@ -4,11 +4,6 @@ NEAREST_COMMON_ANCESTOR=$(git merge-base origin/master HEAD)
 MASTER_COUNT=$(git rev-list --first-parent --count "$NEAREST_COMMON_ANCESTOR")
 HEAD_COUNT=$(git rev-list --first-parent --count HEAD)
 COUNT_DIFF=$((HEAD_COUNT-MASTER_COUNT))
-BRANCH_NAME=$(git branch --show-current)
+SHORT_COMMIT_HASH=$(git rev-parse --short HEAD)
 
-# If not on a branch, use short commit hash
-if [[ "$BRANCH_NAME" == "" ]]; then
-  BRANCH_NAME=$(git rev-parse --short HEAD)
-fi
-
-echo "${MASTER_COUNT}.${COUNT_DIFF}.${BRANCH_NAME}.${DHIFFIE_ENV}"
+echo "${MASTER_COUNT}.${COUNT_DIFF}.${SHORT_COMMIT_HASH}.${DHIFFIE_ENV}"
