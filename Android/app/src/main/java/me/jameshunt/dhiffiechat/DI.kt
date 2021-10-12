621dd53431eb504f401843ac71a866222d6b1ab8
huntj88 <huntj88@gmail.com>
2021-10-11 20:51:05 -0500

    cleanup

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/DI.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/DI.kt
@@ -1,20 +1,20 @@
 package me.jameshunt.dhiffiechat
 
 import android.content.Context
-import android.os.Environment
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.sqldelight.android.AndroidSqliteDriver
 import me.jameshunt.dhiffiechat.crypto.toBase64String
 import me.jameshunt.dhiffiechat.crypto.toDHPublicKey
 import me.jameshunt.dhiffiechat.service.*
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.io.File
 import java.net.URL
 import java.security.PublicKey
 import java.time.Instant
@@ -22,17 +22,18 @@ import java.time.format.DateTimeFormatter
 import java.util.concurrent.TimeUnit
 
 class DI(val application: DhiffieChatApp) {
-    private val cacheDir: File = application.cacheDir
-    private val fileProviderDir: File = File(
-        application.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-        "DhiffieChat"
-    )
-    private val fileLocationUtil = FileLocationUtil(cacheDir, fileProviderDir)
+    private val fileLocationUtil = FileLocationUtil(application)
+
     private val prefManager = PrefManager(
-        prefs = application.getSharedPreferences("dhiffieChat", Context.MODE_PRIVATE)
+        application.getSharedPreferences("dhiffieChat", Context.MODE_PRIVATE)
     )
 
-    private val dbQueryManager = DBQueryManager(application, prefManager)
+    private val db = Database(AndroidSqliteDriver(
+        schema = Database.Schema,
+        context = application,
+        name = "dhiffiechat.db",
+        factory = { SupportFactory(prefManager.getDBPassword().toByteArray()).create(it) }
+    ))
 
     private val moshi: Moshi = Moshi.Builder()
         .add(object {
@@ -50,13 +51,13 @@ class DI(val application: DhiffieChatApp) {
         .add(object {
             @ToJson
             fun toJson(publicKey: PublicKey): String = publicKey.toBase64String()
-            @FromJson
-            fun fromJson(base64: String): PublicKey = base64.toDHPublicKey() // TODO: re-evaluate, cause rsa keys now too
+            @FromJson // TODO: re-evaluate, cause rsa keys now too
+            fun fromJson(base64: String): PublicKey = base64.toDHPublicKey()
         })
         .addLast(KotlinJsonAdapterFactory())
         .build()
 
-    private val identityManager = IdentityManagerImpl(dbQueryManager.getEncryptionKeyQueries())
+    private val identityManager: IdentityManager = IdentityManagerImpl(db.encryption_keyQueries)
     private val authManager = ServerAuthManager(identityManager, moshi)
     private val headerInterceptor = HeaderInterceptor(identityManager, authManager)
 
@@ -77,13 +78,13 @@ class DI(val application: DhiffieChatApp) {
         .build()
 
     private val api = retrofit.create(LambdaApi::class.java)
-    private val remoteFileService = RemoteFileServiceImpl(okhttp)
+    private val remoteFileService: RemoteFileService = RemoteFileServiceImpl(okhttp)
     private val userService = UserService(
-        dbQueryManager.getAliasQueries(), api, authManager, identityManager, prefManager
+        db.aliasQueries, api, authManager, identityManager, prefManager
     )
 
-    private val ephemeralKeyService = EphemeralKeyServiceImpl(
-        encryptionKeyQueries = dbQueryManager.getEncryptionKeyQueries()
+    private val ephemeralKeyService: EphemeralKeyService = EphemeralKeyServiceImpl(
+        db.encryption_keyQueries
     )
 
     private val messageService = MessageService(
@@ -96,7 +97,7 @@ class DI(val application: DhiffieChatApp) {
     )
 
     private val ephemeralKeySyncService =
-        EphemeralKeySyncService(api, dbQueryManager.getEncryptionKeyQueries(), identityManager)
+        EphemeralKeySyncService(api, db.encryption_keyQueries, identityManager)
 
     private val initService = InitService(api, userService, ephemeralKeySyncService)
 
