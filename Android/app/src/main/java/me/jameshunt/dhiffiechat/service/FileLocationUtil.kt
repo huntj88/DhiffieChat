889443d4dfcca6a000ca0035f1d2b9e5ddad4447
huntj88 <huntj88@gmail.com>
2021-10-11 20:51:05 -0500

    cleanup

--- Android/app/src/main/java/me/jameshunt/dhiffiechat/service/FileLocationUtil.kt
+++ Android/app/src/main/java/me/jameshunt/dhiffiechat/service/FileLocationUtil.kt
@@ -1,12 +1,16 @@
 package me.jameshunt.dhiffiechat.service
 
+import android.content.Context
+import android.os.Environment
 import android.util.Log
 import java.io.File
 
-class FileLocationUtil(
-    private val cacheDir: File,
-    private val fileProviderDir: File
-) {
+class FileLocationUtil(context: Context) {
+    private val cacheDir: File = context.cacheDir
+    private val fileProviderDir: File = File(
+        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+        "DhiffieChat"
+    )
 
     fun outgoingEncryptedFile(): File {
         val file = File(cacheDir, "send")
